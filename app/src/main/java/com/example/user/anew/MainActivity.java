f3faa837f3406a7fcf7c775c147ef9c4d5245b54
Arad <ag7393@bs.amalnet.k12.il>
2019-02-28 09:42:23 +0200

    njndcnydyi

--- app/src/main/java/com/example/user/anew/MainActivity.java
+++ app/src/main/java/com/example/user/anew/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.user.anew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtxt;
-    Button btnp, btnm, btnk, btnh, btne, btnc, btns;
-
+   public EditText edtxt;
+     double a;
+    boolean plus;
+    double sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         edtxt = (EditText) findViewById(R.id.edtxt);
+    }
+
+    public void plus(View view) {
+        String st=edtxt.getText().toString();
+        edtxt.setText("");
+        
+    }
+
+    public void minus(View view) {
+        String st=edtxt.getText().toString();
+        Double edtxt=Double.parseDouble(st);
+
+
+
+
     }
 }
